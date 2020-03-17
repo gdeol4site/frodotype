127c44cbf5443ff58caa6a448bee24992de6c66d
gdeol4 <gdeol4@uwo.ca>
2020-03-17 12:08:43 -0400

    added smooth scroll

--- gpt2-model/functions.js
+++ gpt2-model/functions.js
@@ -1,13 +1,18 @@
-function openTab(evt, tabName) {
-  var i, x, tablinks;
-  x = document.getElementsByClassName("content-tab");
-  for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tab");
-  for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" is-active", "");
-  }
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " is-active";
-}
+
+// Smooth scroll // https://css-tricks.com/snippets/jquery/smooth-scrolling/
+// https://codepen.io/magglomag/pen/RPGgJG
+// Select all »a« elements with a parent class »links« and add a function that is executed on click
+$( '.links a' ).on( 'click', function(e){
+
+  // Define variable of the clicked »a« element (»this«) and get its href value.
+  var href = $(this).attr( 'href' );
+
+  // Run a scroll animation to the position of the element which has the same id like the href value.
+  $( 'html, body' ).animate({
+		scrollTop: $( href ).offset().top
+  }, '300' );
+
+  // Prevent the browser from showing the attribute name of the clicked link in the address bar
+  e.preventDefault();
+
+});
