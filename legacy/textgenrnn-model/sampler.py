53f3a370fba1855242a77db221390cf2432f61de
Gurkamal Deol <gdeol4@uwo.ca>
2020-07-04 10:46:41 -0400

    Repo clean up

--- legacy/textgenrnn-model/sampler.py
+++
@@ -1,8 +0,0 @@
-from textgenrnn import textgenrnn
-
-textgen = textgenrnn(weights_path='colaboratory_weights.hdf5',
-vocab_path='colaboratory_vocab.json',
-config_path='colaboratory_config.json')
-
-#textgen_weights = textgenrnn('colaboratory_weights.hdf5')
-textgen.generate(5, temperature=0.67)
